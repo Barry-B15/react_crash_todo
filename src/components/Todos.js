4370cb9ce64a0b454a8451fb9b8e9f8a268f5b79
Barry <Barry-B15@users.noreply.github.com>
2020-08-30 22:10:04 +0900

    feat: loop thru todo list

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
+import TodoItem from "./TodoItem";
 
 // video format
 class Todos extends Component {
     render() {
         console.log(this.props.todos);
-        return ( <
-            div >
-            <
-            h1 > Todos < /h1> <
-            /div>
-        )
+        return this.props.todos.map((todo) => ( <
+            TodoItem key = { todo.id }
+            todo = { todo }
+            / >
+        ));
     }
 }
 // function Todos() { // original code format
